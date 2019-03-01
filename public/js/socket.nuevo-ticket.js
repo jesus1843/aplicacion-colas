56eda70ddd8e3b94dd0a4a7da619b5d0f4ae73fb
J Jesus Villanueva Jr <jesus1843@hotmail.com>
2019-03-01 11:54:15 -0600

    Primer Commit - Producción

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,21 @@
+// Comando para establecer la conexión
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', () => {
+  console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', () => {
+  console.log('Desconectado al servidor');
+});
+
+socket.on('estadoActual', (resp) => {
+  label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+  socket.emit('siguienteTicket', null, function(siguienteTicket) {
+    label.text(siguienteTicket);
+  });
+});
